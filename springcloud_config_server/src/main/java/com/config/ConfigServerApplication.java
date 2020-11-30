1bbd888817b1e705a6a8a0f1a006443c7b51347f
zhou <503175675@qq.com>
2020-11-30 14:18:56 +0800

    配置中心

---
+++ springcloud_config_server/src/main/java/com/config/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author ZYH
+ * @version 5.0.0
+ * created at 2020/11/30 14:08
+ * SpringCloud配置分布式服务中心
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
